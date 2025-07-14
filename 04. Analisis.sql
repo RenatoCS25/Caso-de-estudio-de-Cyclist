cdf3a1cdfd59838fb52e64d4fe853d83b99a1e11
RenatoCS <renatocahuanasilva@gmail.com>
2025-07-14 17:20:11 -0500

    Update 04. Analisis.sql

--- 04. Analisis.sql
+++ 04. Analisis.sql
@@ -1,50 +1,49 @@
--- bikes types used by riders
+-- tipos de bicicletas utilizadas por los ciclistas
 
 SELECT member_casual, rideable_type, COUNT(*) AS total_trips
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY member_casual, rideable_type
 ORDER BY member_casual, total_trips;
 
--- no. of trips per month
+-- número de viajes por mes
 
 SELECT month, member_casual, COUNT(ride_id) AS total_trips
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY month, member_casual
 ORDER BY member_casual;
 
--- no. of trips per day of week
+-- número de viajes por día de la semana
 
 SELECT day_of_week, member_casual, COUNT(ride_id) AS total_trips
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY day_of_week, member_casual
 ORDER BY member_casual;
 
--- no. of trips per hour
-
+-- número de viajes por hora
 SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, COUNT(ride_id) AS total_trips
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY hour_of_day, member_casual
 ORDER BY member_casual;
 
--- average ride_length per month
+-- duración promedio del viaje por mes
 
 SELECT month, member_casual, AVG(ride_length) AS avg_ride_duration
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY month, member_casual;
 
--- average ride_length per day of week
+-- duración promedio del viaje por día de la semana
 
 SELECT day_of_week, member_casual, AVG(ride_length) AS avg_ride_duration
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY day_of_week, member_casual;
 
--- average ride_length per hour
+-- duración media del viaje por hora
 
 SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, AVG(ride_length) AS avg_ride_duration
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY hour_of_day, member_casual;
 
--- starting station locations
+-- ubicaciones de las estaciones de inicio
 
 SELECT start_station_name, member_casual,
   AVG(start_lat) AS start_lat, AVG(start_lng) AS start_lng,
@@ -52,8 +51,7 @@ SELECT start_station_name, member_casual,
 FROM [2024_tripdata.cleaned_combined_data]
 GROUP BY start_station_name, member_casual;
 
--- ending station locations
-
+-- ubicaciones de estaciones finales
 SELECT end_station_name, member_casual,
   AVG(end_lat) AS end_lat, AVG(end_lng) AS end_lng,
   COUNT(ride_id) AS total_trips
